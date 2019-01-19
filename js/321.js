bae7a9659b8c769a27f75af95d9efdef4de81748
GeorgeTyupin <44722304+GeorgeTyupin@users.noreply.github.com>
2019-01-19 13:37:50 +0300

    Add files via upload

---
+++ js/321.js
@@ -0,0 +1,11 @@
+function plus(){
+    
+var a = document.getElementById('a').value;   
+var b = document.getElementById('b').value;    
+    
+var ans = parseInt(a)+parseInt(b);     
+    
+    
+    
+document.getElementById('ans').innerHTML = ans ;
+}
